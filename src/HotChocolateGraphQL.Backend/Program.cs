4162ed0b02ef597ea5bb3bbe7b9ac9ab1b44b6ed
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-03-21 13:36:26 -0700

    Add Lambda Support

--- src/HotChocolateGraphQL.Backend/Program.cs
+++ src/HotChocolateGraphQL.Backend/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGraphQLServer()
 				.AddQueryType<Query>();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 var app = builder.Build();
 app.UseRouting()
 	.UseEndpoints(endpoints => endpoints.MapGraphQL());
